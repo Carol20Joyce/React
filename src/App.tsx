c15ec4424dcc4f3555fecfc3b2602648e02c16b1
Joyce Caroline Rodrigues <carolinerodriguesjoyce@gmail.com>
2023-05-22 15:02:22 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
-import {Grid} from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './componentes/temas/listaTemas/ListaTemas';
+import ListaPostagem from './componentes/postagens/listaPostagem/ListaPostagem';
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/home" element={<Home />}/>
-          </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  )
-};
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/postagens" element={<ListaPostagem />} />
+
+
+      </Routes>
+      <Footer />
+    </Router>
+
+  );
+}
 
 export default App;
